74d68a29a9dbe8c1e9ab9f1612f513bb2f4cee53
Irene Perez <inreallifeirene@users.noreply.github.com>
2020-10-30 16:41:11 -0700

    Created navbar

---
+++ src/components/SideMenu.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import Menu from "semantic-ui-react"
+
+const SideMenu = () => {
+  return (
+    <Menu pointing secondary vertical>
+      <Menu.Item name="Overview" as={Link} to="/" activeClassName="active" />
+      <Menu.Item name="Animals" as={Link} to="/animals" activeClassName="active" />
+      <Menu.Item name="Astronomy" as={Link} to="/astronomy" activeClassName="active" />
+      <Menu.Item name="Flowers" as={Link} to="/flowers" activeClassName="active" />
+    </Menu>
+  )
+}
+
+export default SideMenu
