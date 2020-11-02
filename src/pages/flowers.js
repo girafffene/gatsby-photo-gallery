720376a889ae16f03502f0a0fdd8c69a8887c18a
Irene Perez <inreallifeirene@users.noreply.github.com>
2020-11-02 11:51:22 -0800

    created a flowers album

---
+++ src/pages/flowers.js
@@ -0,0 +1,42 @@
+//imported dependencies
+import React from "react"
+import { graphql } from "gatsby"
+//import styles
+import { Header } from "semantic-ui-react"
+//imported components
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import PhotoGallery, { photoMapper } from "../components/Gallery"
+
+const FlowersPage = ({ data }) => {
+  const { edges } = data.allStrapiImages;
+  const photos = photoMapper(edges);
+
+  return (
+    <Layout>
+      <SEO title="Flowers" description="Flowers photo album" />
+      <Header as="h1">Flowers</Header>
+      <PhotoGallery photos={photos} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allStrapiImages(filter: {tags: {name: {eq: "flowers"}}}) {
+      edges {
+        node {
+          url {
+            childImageSharp {
+              fixed(width: 960) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default FlowersPage
