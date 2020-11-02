e5c13169f9facacba14a9def8c6ccca71300715c
Irene Perez <inreallifeirene@users.noreply.github.com>
2020-11-02 11:51:41 -0800

    created an animals album

---
+++ src/pages/animals.js
@@ -0,0 +1,42 @@
+//imported dependencies
+import React from "react"
+import { graphql } from "gatsby"
+//imported styles
+import { Header } from "semantic-ui-react"
+//imported components
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import PhotoGallery, { photoMapper } from "../components/Gallery"
+
+const AnimalsPage = ({ data }) => {
+  const { edges } = data.allStrapiImages;
+  const photos = photoMapper(edges);
+
+  return (
+    <Layout>
+      <SEO title="Animals" description="Animals photo album" />
+      <Header as="h1">Animals</Header>
+      <PhotoGallery photos={photos} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allStrapiImages(filter: {tags: {name: {eq: "animals"}}}) {
+      edges {
+        node {
+          url {
+            childImageSharp {
+              fixed(width: 960) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default AnimalsPage
